48cb6722c4a8d647b647032b810c4a10b30d977b
Anthonylog13 <104604506+Anthonylog13@users.noreply.github.com>
2024-04-16 17:59:05 -0500

    Public y private

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,30 @@
 
+import { Routes, Route } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { PublicRouter } from './PublicRouter'
+import { GamesRouter } from '../games/routes/GamesRouter'
+import { PrivateRouter } from './PrivateRouter'
+
+
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+   <>
+    <Routes>
+      <Route   
+        path='/login' 
+        element={
+          <PublicRouter>
+            <LoginPage />
+          </PublicRouter>
+        }
+      />
+
+      <Route path='/*' element={
+        <PrivateRouter>
+          <GamesRouter /> //puede dar error
+        </PrivateRouter>
+      } />    
+    </Routes>
+   </>
   )
 }
