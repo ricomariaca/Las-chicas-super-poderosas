76a3bc05c02864d5a02a0ade9babf21d75d9cbff
Anthonylog13 <104604506+Anthonylog13@users.noreply.github.com>
2024-04-15 19:50:08 -0500

    Directory distribution

---
+++ src/ProductHuntApp.jsx
@@ -0,0 +1,5 @@
+export const ProductHuntApp = () => {
+  return (
+    <div>ProductHuntApp</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { ProductHuntApp } from './ProductHuntApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <ProductHuntApp/>
   </React.StrictMode>,
 )
