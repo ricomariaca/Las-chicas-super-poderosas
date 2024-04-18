96a61456a35dc3c432c01b1f7b2d8cdbbe23dfd8
unknown <ricomariaca@gmail.com>
2024-04-17 22:29:32 -0500

    creating the firts part of the product view

--- src/hunt/pages/ProductView.jsx
+++ src/hunt/pages/ProductView.jsx
@@ -1,21 +1,72 @@
 import { ImgGallery } from "../../ui/components/ImgGallery"
 import { Navbar } from "../../ui/components/Navbar"
 import { StarRating } from "../../ui/components/StarRating"
-
+import { BsChatRight } from "react-icons/bs";
+import { CiUser } from "react-icons/ci";
 export const ProductView = () => {
   return (
     <>
       <div>
         <Navbar />
-        <h1>que riko la monda</h1>
-      </div>
-      <div>
-        <ImgGallery />
       </div>
 
-      <div>
-        <StarRating />
+      <div
+        className="bg-teal-600 h-5">
       </div>
+
+      <div className="grid grid-cols-4">
+        <div
+          className="col-span-2">
+          <ImgGallery />
+        </div>
+
+        <div className="my-14 ">
+          <h1 className="font-bold">
+            Title
+          </h1>
+          <span>montana barata</span>
+
+          <div className="my-8">
+            <div>
+              <BsChatRight />
+            </div>
+            <div className="ms-5 ">
+              <StarRating />
+            </div>
+
+          </div>
+          <div className="my8">
+            <p className="font-bold">User</p>
+            <span>juan patricio de montana</span>
+          </div>
+          <div className="my-8">
+            <p className="font-bold">description</p>
+            <span>una montana barata para construir terrenos</span>
+          </div>
+
+        </div>
+        <button>asdsdd</button>
+      </div>
+
+      <div
+        className="bg-teal-600 h-0.5">
+      </div>
+
+      <div className="grid grid-cols-2">
+
+        <div className="my-3 ml-10 ">
+          <CiUser size={30} />
+          <StarRating />
+          <input type="text" placeholder="Ingrese su comentario" />
+        </div>
+        <div className="my-3">
+          <button>asdsd</button>
+        </div>
+      </div>
+      <div
+        className="bg-teal-600 h-0.5">
+      </div>
+
 
 
     </>
