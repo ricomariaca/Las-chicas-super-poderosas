eaf5f0f86364284bd4419a0cf69db21c32a0cae7
Anthonylog13 <104604506+Anthonylog13@users.noreply.github.com>
2024-04-18 19:59:33 -0500

    Finished banner

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,78 +1,90 @@
-import { Link, NavLink, useNavigate } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../auth";
 import { ProductHome } from "../../hunt/pages/ProductHome";
 
-
 export const Navbar = () => {
   const { user, logout, login, logged } = useContext(AuthContext);
-  
 
   const navigate = useNavigate();
   const onLogin = () => {
-    <ProductHome />
-
+    <ProductHome />;
   };
 
   const onLogout = () => {
     logout();
 
-
-
-    navigate('/home', {
-      replace: true
+    navigate("/home", {
+      replace: true,
     });
-  }
+  };
 
   return (
     <>
-      <nav className="bg-white text-green-500 py-4 px-6 flex justify-between items-center">
-        <Link to="/" className="text-lg font-bold">Product hunt</Link>
+      <nav className="bg-white py-4 px-6 flex justify-between items-center">
+        <Link to="/" className="text-lg font-bold text-black">
+          Product hunt
+        </Link>
 
         <div className="flex items-center">
           <ul className="flex justify-center space-x-6">
-
             <li>
-              <NavLink to="/addPoduct" className="nav-link"> agragar producto </NavLink>
+              <NavLink to="/addPoduct" className="nav-link text-teal-600">
+                Agregar producto
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/home" className="nav-link">Inicio</NavLink>
+              <NavLink to="/home" className="nav-link text-teal-600">
+                Inicio
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/perfil" className="nav-link">Perfil</NavLink>
+              <NavLink to="/perfil" className="nav-link text-teal-600">
+                Perfil
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/editprofile" className="nav-link">Editar perfil</NavLink>
+              <NavLink to="/editprofile" className="nav-link text-teal-600">
+                Editar perfil
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/productView" className="nav-link">Vista Perfil</NavLink>
+              <NavLink to="/productView" className="nav-link text-teal-600">
+                Vista Perfil
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/lista productos" className="nav-link">Lista productos</NavLink>
+              <NavLink to="/lista productos" className="nav-link text-teal-600">
+                Lista productos
+              </NavLink>
             </li>
             {!logged && (
-                <li>
-                  <NavLink to="/login" className="nav-link">Iniciar sesion</NavLink>
-                </li>
+              <li>
+                <NavLink to="/login" className="nav-link text-teal-600">
+                  Iniciar sesión
+                </NavLink>
+              </li>
             )}
-
           </ul>
           {logged && (
-              <>
-               <li>
-                  <NavLink to="/login" className="nav-link">Cargar</NavLink>
-                </li>
-             
-                <span className="mr-4">{user?.name}</span>
-                <button onClick={() => onLogout()} className="bg-green-500 text-white px-4 py-2 rounded-lg">
-                  Logout
-                </button>
-              </>
-          )}  
+            <>
+              <li>
+                <NavLink to="/login" className="nav-link text-teal-600">
+                  Cargar
+                </NavLink>
+              </li>
+
+              <span className="mr-4 text-teal-600">{user?.name}</span>
+              <button
+                onClick={() => onLogout()}
+                className="text-teal-600 px-4 py-2 rounded-lg"
+              >
+                Logout
+              </button>
+            </>
+          )}
         </div>
       </nav>
-
     </>
-  )
-}
-
+  );
+};
